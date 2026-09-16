c7340b84100519900a97be54f54b91a496d906b0
kenzuko <kenzuko@gmail.com>
2026-09-16 20:00:46 +0700

    feat(pixel): preload shared map artwork

--- phu-quoc-pixel/src/scenes/BootScene.ts
+++ phu-quoc-pixel/src/scenes/BootScene.ts
@@ -1,9 +1,14 @@
 import Phaser from 'phaser';
 import { SceneKeys } from '../app/scene-keys';
+import { assetRegistry } from '../core/assets/AssetRegistry';
 
 export class BootScene extends Phaser.Scene {
   constructor() {
     super(SceneKeys.Boot);
+  }
+
+  preload(): void {
+    assetRegistry.queueGroup(this, 'shell-map');
   }
 
   create(): void {
